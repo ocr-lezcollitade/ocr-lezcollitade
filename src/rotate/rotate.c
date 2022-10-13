ac6ea5d85c5ccef6c1ddefe1184cb3b3bdf59481
Emmanuel <emmanuel.vouillon@epita.fr>
2022-10-13 12:43:57 +0200

    rotate: cleans code and header

--- src/rotate/rotate.c
+++ src/rotate/rotate.c
@@ -19,32 +19,33 @@ SDL_Surface* rotate_surface(SDL_Surface* surface, int deg)
 {
     Uint32* pixels = surface->pixels;
 
-    int w = surface->w;
-    int h = surface->h;
+    int width = surface->w;
+    int height = surface->h;
 
-    int len = surface->w * surface->h;
+    int len = width * height;
 
-    double angle = deg * M_PI / 180;
+    // converts degrees into radians
+    deg = deg * M_PI / 180;
     
-    int midx = w / 2;
-    int midy = h / 2;
+    int midx = width / 2;
+    int midy = height / 2;
 
-    SDL_Surface* new_surface = SDL_CreateRGBSurface(0, w, h, 32, 0, 0, 0, 0);
+    SDL_Surface* new_surface = SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0);
 
     Uint32* new_pixels = new_surface->pixels;
    
     for (int i = 0; i < len; i++)
     {
-        int x_d = i % w;
-        int y_d = i / w;
+        int destx = i % width;
+        int desty = i / width;
 
-        int x_s = floor(cos(-angle) * (x_d - midx) - sin(-angle) * (y_d - midy)) + midx;
-        int y_s = floor(sin(-angle) * (x_d - midx) + cos(-angle) * (y_d - midy)) + midy;
+        int sourcex = floor(cos(-deg) * (destx - midx) - sin(-deg) * (desty - midy)) + midx;
+        int sourcey = floor(sin(-deg) * (destx - midx) + cos(-deg) * (desty - midy)) + midy;
 
-        int i_s = y_s * w + x_s;
+        int sourcei = sourcey * width + sourcex;
 
-        if (i_s >= 0 && i_s < len)
-            new_pixels[i] = pixels[i_s];
+        if (sourcei >= 0 && sourcei < len)
+            new_pixels[i] = pixels[sourcei];
     }
 
     return new_surface;
@@ -59,7 +60,7 @@ void rotate_image(const char* file, int deg)
 
     SDL_Surface* new_surface = rotate_surface(surface, deg);
 
-    if (SDL_SaveBMP(new_surface, "rotated.bmp") != 0)
+    if (IMG_SavePNG(new_surface, "res.png") != 0)
         errx(EXIT_FAILURE, "%s", SDL_GetError());
 
     SDL_FreeSurface(new_surface);
@@ -67,27 +68,15 @@ void rotate_image(const char* file, int deg)
     SDL_Quit();
 }
 
-int main(int argc, char** argv)
-{
-    if (argc != 3)
-        errx(EXIT_FAILURE, "Usage: ./rotate <file> <angle>");
+//int main(int argc, char** argv)
+//{
+//    if (argc != 3)
+//        errx(EXIT_FAILURE, "Usage: ./rotate <file> <angle>");
+//
+//    int angle = atoi(argv[2]);
+//
+//    rotate_image(argv[1], angle);
+//
+//    return EXIT_SUCCESS;
+//}
 
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
-
-    SDL_Surface* surface = load_image(argv[1]);
-
-    int angle = atoi(argv[2]);
-
-    SDL_Surface* new_surface = rotate_surface(surface, angle);
-
-    if (SDL_SaveBMP(new_surface, "rotated.bmp") != 0)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
-
-    SDL_FreeSurface(new_surface);
-    SDL_FreeSurface(surface);
-
-    SDL_Quit();
-
-    return EXIT_SUCCESS;
-}
--- src/rotate/rotate.h
+++ src/rotate/rotate.h
@@ -2,19 +2,18 @@
 #define ROTATE_ROTATE_H
 
 /**
- * \brief Applies a given rotation to a given surface
+ * \brief Creates a rotated clone of a given surface
  * \fn SDL_Surface* rotate_surface(SDL_Surface* surface, int deg);
- * \param surface; the surface to rotate
- * \param deg; the angle of the rotation
- * \return the a new rotated surface
+ * \param surface The surface to rotate
+ * \param deg The angle of the rotation
+ * \return A new rotated surface
  */
 SDL_Surface* rotate_surface(SDL_Surface* surface, int deg);
 
 /**
- * \brief Applies a given rotation to a image
+ * \brief Creates a rotated clone (res.png) of a given image
  * \fn void rotate_image(const char* file, int deg);
- * \param file; the path to the file to the rotate
- * \param deg; the angle of the rotation
- * \return void
+ * \param file The path to the image to the rotate
+ * \param deg The angle of the rotation
  */
 void rotate_image(const char* file, int deg);
