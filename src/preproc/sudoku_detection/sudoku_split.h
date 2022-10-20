7ccfdfbab304d3bcfd3c61a0df0adcb4b50bb680
Lucas Tilly <lucas.tilly@epita.fr>
2022-10-20 12:46:29 +0200

    Resolve "splitting of the picture"

---
+++ src/preproc/sudoku_detection/sudoku_split.h
@@ -0,0 +1,14 @@
+#ifndef SUDOKU_SPLIT_H
+#define SUDOKU_SPLIT_H
+
+/**
+ * \brief               splits the sudoku grid into 81 squares
+ * \fn                  void sudoku_split(char *black_white, char *grayscale)
+ * \param black_white   the path to the black & white picture of the sudoku
+ * \param grayscale     the path to the grayscaled picture of the sudoku
+ *
+ */
+
+void sudoku_split(char *black_white, char *grayscale);
+
+#endif
